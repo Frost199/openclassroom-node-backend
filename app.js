93fd82245630b9a52db00bce42495b6cfdd9e3f2
Frost199 <emmaldini12@gmail.com>
2019-11-05 08:17:25 +0100

    implemented multer for post route in file uploads

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 //Routes
 const stuffRoutes = require('./routes/stuff');
@@ -32,6 +33,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
 
--- routes/stuff.js
+++ routes/stuff.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const stuffController  = require('../controller/stuff');
-const auth = require('../middleware/auth');
 const router = express.Router();
 
+//middleware
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer_config');
+
 //Create New thing
-router.post('/', auth, stuffController.createThing);
+router.post('/', auth, multer, stuffController.createThing);
 
 //view all about a particular thing
 router.get('/:id', auth, stuffController.getOneThing);
 
 //Update a particular thing
-router.put('/:id', auth, stuffController.modifyThing);
+router.put('/:id', auth, multer, stuffController.modifyThing);
 
 //delete a particular thing
 router.delete('/:id', auth, stuffController.deleteThing);
